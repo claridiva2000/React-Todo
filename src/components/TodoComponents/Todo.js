379d4960f9da37ca0671189f1785f1dbda3956ff
claridiva2000 <claridiva2000@gmail.com>
2019-03-05 22:45:44 -0600

    initial commit

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Todo(props) {
+return(
+  <div className="todo-card">
+  
+  
+  
+  </div>
+)
+
+}
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,59 @@
+import React, { Component } from "react";
+import Todo from "./Todo"
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const tasks= [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+class TodoList extends React.Component {
+constructor(){
+  super();
+  this.state = {
+    
+    
+  }
+}
+
+
+
+render() {
+  return(
+    <div>
+      <h1>My Todo App</h1>
+    <form>
+      <input 
+      task="task"
+      value={this.state.task}
+      placeholder="Task"
+      />
+      <button>Add Todo</button> <button>Clear Completed</button> 
+    </form>
+
+
+    </div>
+  
+  
+  );
+}
+
+
+
+
+
+
+
+}
+
+export default TodoList;
